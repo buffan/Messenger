4350195182977aa2a3c19a55abbad9eb2b32e5ce
buffan <andrew.t.buff@gmail.com>
2017-01-09 14:29:27 -0500

    Renamed variables to more accurately reflect ability to send non-nominations messages

--- Notifier.py
+++ Notifier.py
@@ -29,14 +29,14 @@ class AutoHideScrollbar(Scrollbar):
         raise TclError("cannot use place with this widget")
 
 
-def ProcessNominations(*args):
+def ProcessNames(*args):
     '''
-    Processes each name in nominations list, searches for matching name in friends list.
+    Processes each name in given csv, searches for matching name in friends list.
     Performs different actions depending on how many matching names are found.
     '''
     # Ask user if they still want to send message without {positions} key
-    if '{positions}' not in boilerplate_entry.get('1.0', 'end-1c'):
-        check = '{positions} not found in boilerplate text. Still send message?'
+    if '{positions}' not in message_entry.get('1.0', 'end-1c'):
+        check = '{positions} not found in message body text. Still send message?'
         # Return if user does not want to send message
         if not messagebox.askyesno(title='Send message?', message=check):
             return
@@ -56,26 +56,26 @@ def ProcessNominations(*args):
         return
 
     # Intending to send position notifications
-    if '{positions}' in boilerplate_entry.get('1.0', 'end-1c'):
-        nominations = CompileNominations()
+    if '{positions}' in message_entry.get('1.0', 'end-1c'):
+        people = CompileNominations()
     # Sending positionless message
     else:
-        nominations = CompileNames()
+        people = CompileNames()
 
     # --------------------------- Begin progress window setup ---------------------------
     progress_window = Toplevel()
     progress_window.title('Progress')
     progress = StringVar()
-    progress.set('Processed 0/{}'.format(len(nominations.keys())))
+    progress.set('Processed 0/{}'.format(len(people.keys())))
     ttk.Label(progress_window, textvar=progress).grid(row=0, column=0)
     for child in progress_window.winfo_children():
         child.grid_configure(padx=2, pady=2)
     # --------------------------- End progress window setup------------------------------
 
     not_found = []
-    for counter, person in enumerate(nominations.keys()):
+    for counter, person in enumerate(people.keys()):
         # Update progress window
-        progress.set('Processed {}/{}'.format(counter, len(nominations.keys())))
+        progress.set('Processed {}/{}'.format(counter, len(people.keys())))
         progress_window.update()
 
         possible_friends = client.getUsers(person)
@@ -88,28 +88,28 @@ def ProcessNominations(*args):
             continue
         # If multiple matches found on friends list
         elif len(possible_friends) > 1:
-            DisambiguateFriends(possible_friends, person, nominations, client)
+            DisambiguateFriends(possible_friends, person, people, client)
         # If only 1 match found on friends list
         else:
             friend_uid = possible_friends[0].uid
-            SendMessage(person, friend_uid, nominations, client)
+            SendMessage(person, friend_uid, people, client)
     progress_window.destroy()
 
     # If some users were not found on friends list, display warning box with their names and positions
     if not_found:
-        DisplayNotFound(not_found, nominations)
+        DisplayNotFound(not_found, people)
 
     messagebox.showinfo(title='Done!', message='All messages sent. Program will now exit.')
     root.destroy()
 
 
-def DisplayNotFound(not_found, nominations):
+def DisplayNotFound(not_found, people):
         '''
         Displays a window with all names not found
         '''
         # -------------------------- Begin picture selection window setup ----------------------------
         not_found_window = Toplevel(mainframe)
-        not_found_window.geometry(CalcWindowDimensions(not_found, nominations))
+        not_found_window.geometry(CalcWindowDimensions(not_found, people))
         not_found_window.title('User{} not found'.format('s' if len(not_found) > 1 else ''))
         # Canvas and frame both required for scrollbar
         canvas = Canvas(not_found_window, borderwidth=0)
@@ -134,7 +134,7 @@ def DisplayNotFound(not_found, nominations):
 
         # Display list of names not found and associated positions
         header = 'The following not found. Please send their messages manually.\n\n'
-        names_and_positions = '\n\n'.join(['{} - {}'.format(person, ', '.join(nominations[person])) for person in not_found])
+        names_and_positions = '\n\n'.join(['{} - {}'.format(person, ', '.join(people[person])) for person in not_found])
         Label(frame, text=header, font='Helvetica 14 bold').grid(row=0, column=0, sticky=(E, W))
         Label(frame, text=names_and_positions, anchor='w', justify='left').grid(row=1, column=0, sticky=(E, W))
 
@@ -143,7 +143,7 @@ def DisplayNotFound(not_found, nominations):
         mainframe.wait_window(not_found_window)
 
 
-def CalcWindowDimensions(not_found, nominations):
+def CalcWindowDimensions(not_found, people):
     '''
     Calculates max window dimensions required constrained by screen dimensions
     '''
@@ -153,18 +153,20 @@ def CalcWindowDimensions(not_found, nominations):
     # Values determined by experimentation
     pixels_per_char = 9
     pixels_per_col = 31
-    # header copied from method above. Not the cleanest implementation, passing as a parameter seemed like too much
+    # header text copied from method above. Not the cleanest implementation, but passing as a parameter seemed like too much
     header = 'The following not found. Please send their messages manually.\n\n'
-    names_and_positions_lengths = ([len(', '.join(positions) + name) for name, positions in nominations.items() if name in not_found])
+    # If not sending position messages, positions will be 0-length list
+    names_and_positions_lengths = ([len(', '.join(positions) + name) for name, positions in people.items() if name in not_found])
     # Include header length in calculation
     names_and_positions_lengths.append(len(header))
-    nominations_width = pixels_per_char * max([len(', '.join(positions) + name) for name, positions in nominations.items() if name in not_found])
+    text_width = pixels_per_char * max(names_and_positions_lengths)
     # 2 lines per entry, 1 line of boilerplate, 1 button
-    nominations_height = pixels_per_col * (2 * len(not_found) + 2)
+    text_height = pixels_per_col * (2 * len(not_found) + 2)
 
-    width = min(screen_width, nominations_width)
-    height = min(screen_height, nominations_height)
+    width = min(screen_width, text_width)
+    height = min(screen_height, text_height)
 
+    # Center window on screen
     x_coord = (screen_width - width) //2
     y_coord = (screen_height - height) // 2
 
@@ -178,7 +180,7 @@ def OnFrameConfigure(canvas):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
 
-def DisambiguateFriends(possibilities, person, nominations, client):
+def DisambiguateFriends(possibilities, person, people, client):
     '''
     Displays box of profile pictures. User clicks picture of intended recipient. Message is sent to associated user.
     '''
@@ -201,7 +203,7 @@ def DisambiguateFriends(possibilities, person, nominations, client):
             img = ImageTk.PhotoImage(resized_image)
 
             # I have no idea why this command works. The internet provided the magical lambda uid=uid answer.
-            b = ttk.Button(window, image=img, command=lambda uid=uid: SendAndClose(person, uid, nominations, client, window))
+            b = ttk.Button(window, image=img, command=lambda uid=uid: SendAndClose(person, uid, people, client, window))
             b.grid(column=c, row=r, sticky=(N, S, E, W))
             # Save image reference to prevent garbage collection!
             b.image = img
@@ -213,19 +215,19 @@ def DisambiguateFriends(possibilities, person, nominations, client):
     mainframe.wait_window(window)
 
 
-def SendAndClose(name, uid, nominations, client, window):
+def SendAndClose(name, uid, people, client, window):
     '''
     Destroys window, then calls SendMessage
     '''
     window.destroy()
-    SendMessage(name, uid, nominations, client)
+    SendMessage(name, uid, people, client)
 
 
-def SendMessage(name, uid, nominations, client):
+def SendMessage(name, uid, people, client):
     '''
-    Sends a message to the user with uid containing their nominations.
+    Sends a message to the user with uid.
     '''
-    message = boilerplate_entry.get('1.0', 'end-1c').replace('{positions}', '- ' + '\n- '.join(nominations[name]))
+    message = message_entry.get('1.0', 'end-1c').replace('{positions}', '- ' + '\n- '.join(people[name]))
     print('{}: {}\n'.format(name, message))
     #client.send(uid, message)
 
@@ -241,7 +243,7 @@ def RescaleImage(img):
 
 def CompileNominations():
     '''
-    Opens nominations csv, creates dictionary {person: list of positions}
+    Opens people csv, creates dictionary {person: list of positions}
     Return: {person: positions} dictionary
     '''
     d = defaultdict(set)
@@ -259,7 +261,7 @@ def CompileNominations():
 
 def CompileNames():
     '''
-    Opens nominations csv, creates dictionary {person: ''} for compatibility with program
+    Opens people csv, creates dictionary {person: ''} for compatibility with program
     Return {person: ''} dictionary
     '''
     d = {}
@@ -270,7 +272,8 @@ def CompileNames():
             # Use [:2] to safegaurd against additional fields
             _, person = list(map(str.strip, line.split(',')))[:2]
             # Use title to register the same name regardless of capitalization
-            d[person.title()] = ''
+            # Use empty list for compatibility with .joins used later
+            d[person.title()] = []
     WriteToFile(d)
     return d
 
@@ -278,7 +281,7 @@ def CompileNames():
 def WriteToFile(d):
     '''
     Writes names and associated positions to log file on Desktop.
-    Intended to be more human readable rather than parsed by a program.
+    Human readibility prioritized over computer readibility
     '''
     file_path = path.join(path.expanduser('~'), path.join('Desktop', 'log.txt'))
     with open(file_path, 'w') as f:
@@ -296,6 +299,7 @@ def SetPath(*args):
     result = dlg.show()
 
     if result:
+        # Make field editable only long enough to replace the text
         path_field['state'] = 'normal'
         path_field.delete('1.0', END)
         path_field.insert('1.0', result)
@@ -312,9 +316,10 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
-file_selector = ttk.Button(mainframe, text='Select nominations CSV', command=SetPath).grid(column=1, row=1, sticky=(W, E))
+file_selector = ttk.Button(mainframe, text='Select people/nominations CSV', command=SetPath).grid(column=1, row=1, sticky=(W, E))
 path_field = Text(mainframe, width=30, height=1)
 path_field.grid(column=1, row=2, sticky=(W, E))
+# Make field non-editable
 path_field['state'] = 'disabled'
 
 email = StringVar()
@@ -330,15 +335,16 @@ ttk.Label(mainframe, text='Email').grid(column=2, row=3, sticky=(W, E))
 ttk.Label(mainframe, text='Password').grid(column=2, row=4, sticky=(W, E))
 
 bp_dims = (30, 10)
-boilerplate_entry = Text(mainframe, width=bp_dims[0], height=bp_dims[1], wrap='word')
-boilerplate_entry.grid(column=3, row=1, sticky=(W, E, N, S), columnspan=1, rowspan=3)
+message_entry = Text(mainframe, width=bp_dims[0], height=bp_dims[1], wrap='word')
+message_entry.grid(column=3, row=1, sticky=(W, E, N, S), columnspan=1, rowspan=3)
 ttk.Label(mainframe, text='Message body').grid(column=3, row=4)
 
-ttk.Button(mainframe, text='Send message', command=ProcessNominations).grid(column=3, row=5, sticky=(W, E))
+ttk.Button(mainframe, text='Send message', command=ProcessNames).grid(column=3, row=5, sticky=(W, E))
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
 email_entry.focus()
+# -------------------- End GUI setup --------------------------
 
 root.mainloop()
